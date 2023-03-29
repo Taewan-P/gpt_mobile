146fdf17837f8488a4f1da5d6a3e936968fb8421
Taewan Park <swimingtw@gmail.com>
2023-03-29 18:18:52 +0900

    Add firebase library
    Co-Authored-By: 김다예 <63107805+kimdaye77@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gpt_mobile/screens/login_page.dart';
 import 'package:gpt_mobile/styles/color_schemes.g.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
