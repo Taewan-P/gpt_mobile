cc294133d847ac9b2a8e2d939d39135f7303c75a
Taewan Park <swimingtw@gmail.com>
2024-05-21 02:46:40 +0900

    Add start screen ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
 
     // DataStore
     implementation(libs.androidx.datastore)
--- app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/startscreen/StartScreenActivity.kt
+++ app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/startscreen/StartScreenActivity.kt
@@ -4,12 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.chungjungsoo.gptmobile.R
 import dev.chungjungsoo.gptmobile.presentation.theme.GPTMobileTheme
 
 class StartScreenActivity : ComponentActivity() {
@@ -19,13 +37,72 @@ class StartScreenActivity : ComponentActivity() {
         setContent {
             GPTMobileTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(text = "Hello", modifier = Modifier.padding(innerPadding))
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState()),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        StartScreenLogo()
+                        Spacer(modifier = Modifier.weight(1f))
+                        WelcomeText()
+                        GetStartedButton()
+                    }
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun StartScreenLogo() {
+fun StartScreenLogo(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.gpt_mobile_start_screen),
+        contentDescription = stringResource(R.string.gpt_mobile_introduction_logo),
+        contentScale = ContentScale.FillHeight,
+        modifier = modifier
+            .padding(top = 50.dp)
+            .height(400.dp)
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeText(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(20.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(4.dp)
+                .semantics { heading() },
+            text = stringResource(R.string.welcome_title),
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Text(
+            modifier = Modifier.padding(4.dp),
+            text = stringResource(R.string.welcome_description),
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Preview
+@Composable
+fun GetStartedButton(modifier: Modifier = Modifier) {
+    GPTMobileTheme {
+        Button(
+            modifier = modifier
+                .padding(20.dp)
+                .fillMaxWidth()
+                .height(56.dp),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Get Started")
+        }
+    }
 }
