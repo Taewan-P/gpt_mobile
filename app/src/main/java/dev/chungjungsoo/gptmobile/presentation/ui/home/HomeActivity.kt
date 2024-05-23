c0330799549e2448a81e48ae7126101703fb9359
Taewan Park <swimingtw@gmail.com>
2024-05-23 16:41:19 +0900

    Implement basic home screen

--- app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import dev.chungjungsoo.gptmobile.presentation.theme.GPTMobileTheme
 
@@ -21,29 +14,8 @@ class HomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GPTMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    GPTMobileTheme {
-        Greeting2("Android")
     }
 }
---
+++ app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/home/HomeScreen.kt
@@ -0,0 +1,94 @@
+package dev.chungjungsoo.gptmobile.presentation.ui.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.chungjungsoo.gptmobile.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    val scrollable = { false } // TODO: Change this condition
+    val scrollBehavior = if (scrollable()) {
+        TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    } else {
+        TopAppBarDefaults.pinnedScrollBehavior()
+    }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = { HomeTopAppBar(scrollBehavior) },
+        floatingActionButton = { NewChatButton(onClick = { /* TODO */ }) }
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopAppBar(scrollBehavior: TopAppBarScrollBehavior) {
+    LargeTopAppBar(
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            titleContentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        title = {
+            Text(
+                modifier = Modifier.padding(4.dp),
+                text = stringResource(R.string.chats),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        actions = {
+            IconButton(
+                modifier = Modifier.padding(4.dp),
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(imageVector = Icons.Outlined.Settings, contentDescription = stringResource(R.string.settings))
+            }
+        },
+        scrollBehavior = scrollBehavior
+    )
+}
+
+@Preview
+@Composable
+fun NewChatButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    ExtendedFloatingActionButton(
+        modifier = modifier,
+        onClick = { onClick() },
+        icon = { Icon(Icons.Filled.Add, stringResource(R.string.new_chat)) },
+        text = { Text(text = stringResource(R.string.new_chat)) }
+    )
+}
