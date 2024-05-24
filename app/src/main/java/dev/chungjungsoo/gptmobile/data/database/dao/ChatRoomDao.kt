7d6957e28dd80ac5df297ca9a07c7e146ecc09f1
Taewan Park <swimingtw@gmail.com>
2024-05-25 02:10:43 +0900

    Add database DAOs

---
+++ app/src/main/java/dev/chungjungsoo/gptmobile/data/database/dao/ChatRoomDao.kt
@@ -0,0 +1,24 @@
+package dev.chungjungsoo.gptmobile.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import dev.chungjungsoo.gptmobile.data.database.entity.ChatRoom
+
+@Dao
+interface ChatRoomDao {
+
+    @Query("SELECT * FROM chats")
+    suspend fun getChatRooms(): List<ChatRoom>
+
+    @Insert
+    suspend fun addChatRoom(chatRoom: ChatRoom)
+
+    @Update
+    suspend fun editChatRoom(chatRoom: ChatRoom)
+
+    @Delete
+    suspend fun deleteChatRooms(vararg chatRooms: ChatRoom)
+}
---
+++ app/src/main/java/dev/chungjungsoo/gptmobile/data/database/dao/MessageDao.kt
@@ -0,0 +1,24 @@
+package dev.chungjungsoo.gptmobile.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import dev.chungjungsoo.gptmobile.data.database.entity.Message
+
+@Dao
+interface MessageDao {
+
+    @Query("SELECT * FROM messages WHERE chat_id=:chatInt")
+    suspend fun loadMessages(chatInt: Int): List<Message>
+
+    @Insert
+    suspend fun addMessages(vararg messages: Message)
+
+    @Update
+    suspend fun editMessages(vararg message: Message)
+
+    @Delete
+    suspend fun deleteMessages(vararg message: Message)
+}
