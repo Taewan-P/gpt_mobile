8bafc285559aa119b33ccef4d67f89cfab66d73c
Taewan Park <swimingtw@gmail.com>
2024-05-26 00:08:50 +0900

    Add enabled platform type

--- app/src/main/java/dev/chungjungsoo/gptmobile/data/database/ChatDatabase.kt
+++ app/src/main/java/dev/chungjungsoo/gptmobile/data/database/ChatDatabase.kt
@@ -2,12 +2,15 @@ package dev.chungjungsoo.gptmobile.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import dev.chungjungsoo.gptmobile.data.database.dao.ChatRoomDao
 import dev.chungjungsoo.gptmobile.data.database.dao.MessageDao
+import dev.chungjungsoo.gptmobile.data.database.entity.APITypeConverter
 import dev.chungjungsoo.gptmobile.data.database.entity.ChatRoom
 import dev.chungjungsoo.gptmobile.data.database.entity.Message
 
 @Database(entities = [ChatRoom::class, Message::class], version = 1)
+@TypeConverters(APITypeConverter::class)
 abstract class ChatDatabase : RoomDatabase() {
 
     abstract fun chatRoomDao(): ChatRoomDao
--- app/src/main/java/dev/chungjungsoo/gptmobile/data/database/entity/ChatRoom.kt
+++ app/src/main/java/dev/chungjungsoo/gptmobile/data/database/entity/ChatRoom.kt
@@ -3,6 +3,8 @@ package dev.chungjungsoo.gptmobile.data.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import dev.chungjungsoo.gptmobile.data.dto.ApiType
 
 @Entity(tableName = "chats")
 data class ChatRoom(
@@ -13,6 +15,23 @@ data class ChatRoom(
     @ColumnInfo(name = "title")
     val title: String,
 
+    @ColumnInfo(name = "enabled_platform")
+    val enabledPlatform: List<ApiType>,
+
     @ColumnInfo(name = "created_at")
     val createdAt: Long = System.currentTimeMillis()
 )
+
+class APITypeConverter {
+    @TypeConverter
+    fun fromString(value: String): List<ApiType> {
+        val splitted = value.split(',')
+
+        return splitted.map { s -> ApiType.valueOf(s) }
+    }
+
+    @TypeConverter
+    fun fromList(value: List<ApiType>): String {
+        return value.joinToString(",") { v -> v.name }
+    }
+}
