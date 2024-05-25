cd81b4cea0b5e7be0cea9158ddfbdd2d5e7bd5f0
Taewan Park <swimingtw@gmail.com>
2024-05-26 00:10:58 +0900

    Add setting and chat template activity

---
+++ app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/chat/ChatActivity.kt
@@ -0,0 +1,47 @@
+package dev.chungjungsoo.gptmobile.presentation.ui.chat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.chungjungsoo.gptmobile.presentation.theme.GPTMobileTheme
+
+class ChatActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GPTMobileTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting3(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting3(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview3() {
+    GPTMobileTheme {
+        Greeting3("Android")
+    }
+}
