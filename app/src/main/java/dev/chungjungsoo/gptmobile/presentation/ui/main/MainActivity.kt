c71491a55c0347fd816a9bd177a983bb8bc7d9ea
Taewan Park <swimingtw@gmail.com>
2024-05-28 00:21:12 +0900

    Remove unused code

--- app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/dev/chungjungsoo/gptmobile/presentation/ui/main/MainActivity.kt
@@ -7,16 +7,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import dev.chungjungsoo.gptmobile.presentation.theme.GPTMobileTheme
 import dev.chungjungsoo.gptmobile.presentation.ui.home.HomeActivity
 import dev.chungjungsoo.gptmobile.presentation.ui.startscreen.StartScreenActivity
 import kotlinx.coroutines.launch
@@ -56,21 +51,5 @@ class MainActivity : ComponentActivity() {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
         finish()
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GPTMobileTheme {
-        Greeting("Android")
     }
 }
