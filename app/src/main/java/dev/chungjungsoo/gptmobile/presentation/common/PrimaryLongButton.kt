a002decd66a84788aecd50099517dc97301c7641
Taewan Park <swimingtw@gmail.com>
2024-06-11 01:18:38 +0900

    Fix primary long button theme override

--- app/src/main/java/dev/chungjungsoo/gptmobile/presentation/common/PrimaryLongButton.kt
+++ app/src/main/java/dev/chungjungsoo/gptmobile/presentation/common/PrimaryLongButton.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dev.chungjungsoo.gptmobile.presentation.theme.GPTMobileTheme
 
 @Preview
 @Composable
@@ -19,16 +18,14 @@ fun PrimaryLongButton(
     onClick: () -> Unit = {},
     text: String = ""
 ) {
-    GPTMobileTheme {
-        Button(
-            modifier = modifier
-                .padding(20.dp)
-                .fillMaxWidth()
-                .height(56.dp),
-            onClick = onClick,
-            enabled = enabled
-        ) {
-            Text(text = text)
-        }
+    Button(
+        modifier = modifier
+            .padding(20.dp)
+            .fillMaxWidth()
+            .height(56.dp),
+        onClick = onClick,
+        enabled = enabled
+    ) {
+        Text(text = text)
     }
 }
