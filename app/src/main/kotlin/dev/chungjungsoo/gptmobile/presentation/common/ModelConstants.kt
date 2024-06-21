886de4ed8adef725cd7cd7a62cc9bdfca00b39d6
Taewan Park <swimingtw@gmail.com>
2024-06-21 14:44:23 +0900

    Support Claude 3.5 Sonnet

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/presentation/common/ModelConstants.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/presentation/common/ModelConstants.kt
@@ -3,7 +3,7 @@ package dev.chungjungsoo.gptmobile.presentation.common
 object ModelConstants {
     // LinkedHashSet should be used to guarantee item order
     val openaiModels = linkedSetOf("gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo")
-    val anthropicModels = linkedSetOf("claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307")
+    val anthropicModels = linkedSetOf("claude-3-5-sonnet-20240620", "claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307")
     val googleModels = linkedSetOf("gemini-1.5-pro-latest", "gemini-1.5-flash-latest", "gemini-1.0-pro")
 
     const val ANTHROPIC_MAXIMUM_TOKEN = 4096
