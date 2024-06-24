92d46055206a5ec4aa141caae64fc2dff0f5b4da
Taewan Park <swimingtw@gmail.com>
2024-06-24 17:00:45 +0900

    Refactor code format

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/model/DynamicTheme.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/model/DynamicTheme.kt
@@ -1,8 +1,8 @@
 package dev.chungjungsoo.gptmobile.data.model
 
-enum class DynamicTheme(mode: Int) {
-    ON(1),
-    OFF(0);
+enum class DynamicTheme {
+    ON,
+    OFF;
 
     companion object {
         fun getByValue(value: Int) = entries.firstOrNull { it.ordinal == value }
--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/model/ThemeMode.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/model/ThemeMode.kt
@@ -1,9 +1,9 @@
 package dev.chungjungsoo.gptmobile.data.model
 
-enum class ThemeMode(mode: Int) {
-    SYSTEM(0),
-    DARK(1),
-    LIGHT(2);
+enum class ThemeMode {
+    SYSTEM,
+    DARK,
+    LIGHT;
 
     companion object {
         fun getByValue(value: Int) = entries.firstOrNull { it.ordinal == value }
