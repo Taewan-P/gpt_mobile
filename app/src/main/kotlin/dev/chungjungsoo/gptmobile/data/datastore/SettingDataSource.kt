eee7333f5751087674bc545096ae8c99c352c196
Taewan Park <swimingtw@gmail.com>
2024-07-04 22:12:20 +0900

    Add temperature, topP, System prompt parameter to Platform data type

--- app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/datastore/SettingDataSource.kt
+++ app/src/main/kotlin/dev/chungjungsoo/gptmobile/data/datastore/SettingDataSource.kt
@@ -10,9 +10,15 @@ interface SettingDataSource {
     suspend fun updateStatus(apiType: ApiType, status: Boolean)
     suspend fun updateToken(apiType: ApiType, token: String)
     suspend fun updateModel(apiType: ApiType, model: String)
+    suspend fun updateTemperature(apiType: ApiType, temperature: Float)
+    suspend fun updateTopP(apiType: ApiType, topP: Float)
+    suspend fun updateSystemPrompt(apiType: ApiType, prompt: String)
     suspend fun getDynamicTheme(): DynamicTheme?
     suspend fun getThemeMode(): ThemeMode?
     suspend fun getStatus(apiType: ApiType): Boolean?
     suspend fun getToken(apiType: ApiType): String?
     suspend fun getModel(apiType: ApiType): String?
+    suspend fun getTemperature(apiType: ApiType): Float?
+    suspend fun getTopP(apiType: ApiType): Float?
+    suspend fun getSystemPrompt(apiType: ApiType): String?
 }
